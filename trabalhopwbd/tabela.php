3286669599b3b123a19ac83cfbf234a32146d629
jords342 <j0rd4ns4nt0s2021@gmail.com>
2025-07-07 07:49:53 -0300

    update tabela datatables

--- trabalhopwbd/tabela.php
+++ trabalhopwbd/tabela.php
@@ -13,7 +13,10 @@ $conexao = null;
 <head>
     <meta charset="utf-8" />
     <title>Pessoa</title>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.3.2/css/dataTables.dataTables.css" />
+    <script src="https://cdn.datatables.net/2.3.2/js/dataTables.js"></script>
 </head>
 
 <body>
@@ -22,7 +25,7 @@ $conexao = null;
         <hr />
         <a href="index.php" class="btn btn-primary mb-2">Cadastrar</a>
 
-        <table class="table table-bordered">
+        <table class="table table-bordered" id="pessoaTable">
             <thead class="table-dark">
                 <tr>
                     <th>Nome</th>
@@ -46,6 +49,11 @@ $conexao = null;
             </tbody>
         </table>
     </div>
+    <script>
+        $(document).ready(function() {
+            $('#pessoaTable').DataTable();
+        });
+    </script>
 </body>
 
 </html>
