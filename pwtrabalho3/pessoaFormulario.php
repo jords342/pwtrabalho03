87097f49be3f9f96d1eaaae94acd7ada649c9a36
jords342 <j0rd4ns4nt0s2021@gmail.com>
2025-07-14 01:19:37 -0300

    correção layout pagina cadastro pessoa

--- pwtrabalho3/pessoaFormulario.php
+++ pwtrabalho3/pessoaFormulario.php
@@ -21,9 +21,7 @@ if (isset($_GET['idPessoa'])) {
 <head>
     <meta charset="utf-8">
     <title>Cadastro de Pessoa</title>
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-
     <style>
         .error {
             color: red;
@@ -40,22 +38,30 @@ if (isset($_GET['idPessoa'])) {
 
         <form id="formPessoa" action="pessoaSalvar.php" method="post">
             <input type="hidden" name="idPessoa" value="<?= $idPessoa; ?>">
-
-            <div class="mb-3">
-                <label for="nome" class="form-label">Nome</label>
-                <input type="text" name="nome" id="nome" class="form-control" value="<?= $nome; ?>" required>
+            <div class="row">
+                <div class="col-12">
+                    <div class="mb-3">
+                        <label for="nome" class="form-label">Nome</label>
+                        <input type="text" name="nome" id="nome" class="form-control" value="<?= $nome; ?>" required>
+                    </div>
+                </div>
             </div>
-
-            <div class="mb-3">
-                <label for="cpf" class="form-label">CPF</label>
-                <input type="text" name="cpf" id="cpf" class="form-control" value="<?= $cpf; ?>" required>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="mb-3">
+                        <label for="cpf" class="form-label">CPF</label>
+                        <input type="text" name="cpf" id="cpf" class="form-control" value="<?= $cpf; ?>" required>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="mb-3">
+                        <label for="email" class="form-label">E-mail</label>
+                        <input type="email" name="email" id="email" class="form-control" value="<?= $email; ?>"
+                            required>
+                    </div>
+                </div>
             </div>
-
-            <div class="mb-3">
-                <label for="email" class="form-label">E-mail</label>
-                <input type="email" name="email" id="email" class="form-control" value="<?= $email; ?>" required>
-            </div>
-
+            <hr />
             <a href="pessoaTabela.php" class="btn btn-secondary">Voltar</a>
             <button type="submit" class="btn btn-success">Salvar</button>
         </form>
@@ -70,7 +76,6 @@ if (isset($_GET['idPessoa'])) {
     <script>
         $(document).ready(function () {
             $('#cpf').mask('000.000.000-00');
-
             $("#formPessoa").validate({
                 rules: {
                     nome: {
